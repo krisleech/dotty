f1d76db662c89ad75ae711f6ff39f6f332b9e9d3
Kris Leech <kris.leech@gmail.com>
2018-03-30 19:38:40 +0100

    extract JS in to own files

---
+++ resources/public/join.js
@@ -0,0 +1,55 @@
+$(function(){
+    $('#up').click(function(e) {
+        e.preventDefault();
+        sendEvent({ "type": "move", "direction": "up" });
+    })
+
+    $('#down').click(function(e) {
+        e.preventDefault();
+        sendEvent({ "type": "move", "direction": "down" });
+    })
+
+    $('#left').click(function(e) {
+        e.preventDefault();
+        sendEvent({ "type": "move", "direction": "left" });
+    })
+
+    $('#right').click(function(e) {
+        e.preventDefault();
+        sendEvent({ "type": "move", "direction": "right" });
+    })
+})
+
+var socket = new WebSocket('ws://' + window.location.host + '/ws/player');
+
+socket.addEventListener('open', function(event) {
+    console.log('player got open', event);
+});
+
+var playerId;
+
+socket.onmessage = function(message) {
+    event = JSON.parse(message.data);
+    console.log('event received', event)
+
+    switch(event.type) {
+        case "id-created":
+            playerId = event.id;
+            console.log('id set', playerId)
+            break;
+        default:
+            console.log("Unknown event type", event.type)
+    }
+
+
+};
+
+// send message to server
+var sendEvent  = function(event) {
+    if (socket.readyState === WebSocket.OPEN) {
+        event['player-id'] = playerId;
+        console.log('event sent', event);
+        json = JSON.stringify(event);
+        socket.send(json);
+    }
+};
